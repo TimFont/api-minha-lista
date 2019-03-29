e6363770e4cd45f4bd9f18fe285d69cc1f46b673
italo fontes <italo5123@hotmail.com>
2019-03-29 00:14:24 -0300

    cors enabled

--- core/Routes.php
+++ core/Routes.php
@@ -4,6 +4,8 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
 
 
+require __DIR__ . '/../core/Cors.php';
+
 $app->group('/api', function(){
     
     $this->get('/notes', 'App\controllers\NotesController:getAllNotes');
@@ -11,4 +13,9 @@ $app->group('/api', function(){
     $this->get('/notes/{id}', 'App\controllers\NotesController:getById');
     $this->delete('/notes/{id}','App\controllers\NotesController:deleteNote');
     $this->put('/notes/{id}', 'App\controllers\NotesController:editNote');
+});
+
+$app->map(['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], '/{routes:.+}', function($req, $res) {
+    $handler = $this->notFoundHandler; // handle using the default Slim page not found handler
+    return $handler($req, $res);
 });
